d128680bbbbe853eee98c973b897e92c279a6a80
AshleyMariseta <60039809+AshleyMariseta@users.noreply.github.com>
2020-06-28 23:47:58 +0200

    Add files via upload

---
+++ zebra.js
@@ -0,0 +1,13 @@
+var pressed = true;
+
+function zebraOn(){
+
+     
+    var x = document.getElementsByClassName("even");
+        var i;
+
+    for(i=0;i<x.length;i++){
+        x[i].style.backgroundColor="black";
+    };
+   
+}
